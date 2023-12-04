eb0541d1715193ca891afd87dd3d45b7ed132888
Bruno Honda <bruno.honda@live.com>
2023-12-04 20:54:21 -0300

    feat(post): implements post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 import { BioComponent } from './bio/bio.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PostComponent } from './post/post.component';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,16 @@ import { NavbarComponent } from './navbar/navbar.component';
     CommonModule,
     BioComponent,
     NavbarComponent,
+    PostComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent {
+  posts = [
+    { title: '5 dicas para a sua carreira profissional', text: 'Confira algumas dicas que podem ajudar a alavancar sua carreira como desenvolvedor front-end' },
+    { title: '5 dicas para a sua carreira profissional', text: 'Confira algumas dicas que podem ajudar a alavancar sua carreira como desenvolvedor front-end' },
+    { title: '5 dicas para a sua carreira profissional', text: 'Confira algumas dicas que podem ajudar a alavancar sua carreira como desenvolvedor front-end' },
+    { title: '5 dicas para a sua carreira profissional', text: 'Confira algumas dicas que podem ajudar a alavancar sua carreira como desenvolvedor front-end' },
+  ];
+}
