fe21f780465daea32aa305f676fa77683146ac53
Bruno Honda <bruno.honda@live.com>
2023-12-04 20:14:17 -0300

    feat(app): adds navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+import { NavbarComponent } from './navbar/navbar.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
+    NavbarComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
